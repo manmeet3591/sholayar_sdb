7e029c0108e393b952b2d0d55057ae2403d7aa4a
Manmeet Singh <manmeet20singh11@gmail.com>
2023-12-01 03:16:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ df_training = pd.read_csv('https://raw.githubusercontent.com/manmeet3591/sholaya
 # Create Plotly figure
 fig = px.scatter_mapbox(df_training, lat="lat", lon="lon", color="bathy",
                         color_continuous_scale=px.colors.sequential.PuBu, size_max=5, zoom=11,
-                        mapbox_style="carto-positron")
+                        mapbox_style="carto-positron", range_color=[0, 50])
 
 # Display the figure
 st.plotly_chart(fig)
@@ -36,3 +36,16 @@ if st.button('Train Machine Learning Model'):
     # In a real app, replace this with actual model training code
     time.sleep(1)  # Simulating time taken for model training
     st.success('Model training complete.')
+
+# Button to display predicted bathymetry lines
+if st.button('Display Predicted Bathymetry Lines'):
+    # Load predicted dataset
+    df_predicted = pd.read_csv('https://raw.githubusercontent.com/manmeet3591/sholayar_sdb/main/sholayar_full_bathy_predicted.csv')
+
+    # Create Plotly figure for predicted data
+    fig_predicted = px.scatter_mapbox(df_predicted, lat="y", lon="x", color="y_test_pred",
+                                      color_continuous_scale=px.colors.sequential.PuBu, size_max=5, zoom=11,
+                                      mapbox_style="carto-positron", range_color=[0, 50])
+
+    # Display the predicted figure
+    st.plotly_chart(fig_predicted)
